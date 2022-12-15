26ea78a2efddc8fbe89e672edb4a12d08ea8f976
Miguel Rietz <miguel.rietz@netcentric.biz>
2022-12-15 21:53:40 +0100

    initial commit

---
+++ src/test/java/devs/mrp/springturkey/SpringTurkeyApplicationTests.java
@@ -0,0 +1,13 @@
+package devs.mrp.springturkey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTurkeyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
