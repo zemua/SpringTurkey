37efd01d9234eec926c4fda42877de6c94dafe0a
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-05-07 21:00:24 +0200

    user service

--- src/main/java/devs/mrp/springturkey/database/service/impl/UserServiceImpl.java
+++ src/main/java/devs/mrp/springturkey/database/service/impl/UserServiceImpl.java
@@ -1,24 +1,34 @@
 package devs.mrp.springturkey.database.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import devs.mrp.springturkey.components.LoginDetailsReader;
 import devs.mrp.springturkey.database.entity.User;
+import devs.mrp.springturkey.database.repository.UserRepository;
 import devs.mrp.springturkey.database.service.UserService;
 import reactor.core.publisher.Mono;
 
 @Service
 public class UserServiceImpl implements UserService {
 
+	@Autowired
+	private LoginDetailsReader loginDetailsReader;
+
+	@Autowired
+	private UserRepository userRepository;
+
 	@Override
 	public Mono<User> addCurrentUser() {
-		// TODO Auto-generated method stub
-		return null;
+		User user = User.builder()
+				.email(loginDetailsReader.getUsername())
+				.build();
+		return userRepository.save(user);
 	}
 
 	@Override
 	public Mono<User> getUser() {
-		// TODO Auto-generated method stub
-		return null;
+		return userRepository.findById(loginDetailsReader.getUsername());
 	}
 
 }
--- src/test/java/devs/mrp/springturkey/database/service/impl/UserServiceImplTest.java
+++ src/test/java/devs/mrp/springturkey/database/service/impl/UserServiceImplTest.java
@@ -1,14 +1,61 @@
 package devs.mrp.springturkey.database.service.impl;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentMatchers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import devs.mrp.springturkey.components.impl.LoginDetailsReaderImpl;
+import devs.mrp.springturkey.database.entity.User;
+import devs.mrp.springturkey.database.repository.UserRepository;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {LoginDetailsReaderImpl.class, UserServiceImpl.class})
 class UserServiceImplTest {
 
+	@MockBean
+	private UserRepository userRepository;
+
+	@Autowired
+	private UserServiceImpl userServiceImpl;
+
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	@WithMockUser("user@mail.me")
+	void testAddCurrentUser() {
+		User userIn = User.builder().email("user@mail.me").build();
+		User userOut = User.builder().id("userId").email("user@mail.me").build();
+
+		when(userRepository.save(ArgumentMatchers.refEq(userIn))).thenReturn(Mono.just(userOut));
+
+		Mono<User> monoUser = userServiceImpl.addCurrentUser();
+
+		StepVerifier.create(monoUser)
+		.expectNext(userOut)
+		.expectComplete()
+		.verify();
+	}
+
+	@Test
+	@WithMockUser("user@mail.me")
+	void testGetUser() {
+		User userOut = User.builder().id("userId").email("user@mail.me").build();
+
+		when(userRepository.findById("user@mail.me")).thenReturn(Mono.just(userOut));
+
+		Mono<User> monoUser = userServiceImpl.getUser();
+
+		StepVerifier.create(monoUser)
+		.expectNext(userOut)
+		.expectComplete()
+		.verify();
 	}
 
 }
