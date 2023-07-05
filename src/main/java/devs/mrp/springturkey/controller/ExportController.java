bee0a72a642de021edd37607644dd7fee8e2f996
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-07-05 21:55:30 +0200

    exporting bulk dto in controller WIP

--- src/main/java/devs/mrp/springturkey/controller/ExportController.java
+++ src/main/java/devs/mrp/springturkey/controller/ExportController.java
@@ -1,21 +1,26 @@
 package devs.mrp.springturkey.controller;
 
-import java.util.Map;
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import devs.mrp.springturkey.controller.dto.ExportDataDto;
+import devs.mrp.springturkey.database.service.FullUserDumpFacade;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(path = "/export")
 public class ExportController {
 
+	@Autowired
+	private FullUserDumpFacade dumpFacade;
+
 	@GetMapping("/full")
-	public Mono<ResponseEntity<Map<Object,Object>>> fullExport() {
-		throw new RuntimeException("Not yet implemented");
+	public Mono<ResponseEntity<ExportDataDto>> fullExport() {
+		return Mono.just(new ResponseEntity<ExportDataDto>(new ExportDataDto(), HttpStatusCode.valueOf(200)));
 	}
 
 }
