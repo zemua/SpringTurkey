e9b476aa250ef3d12e2af61005837d19579f0aa9
Miguel Rietz <miguel.rietz@netcentric.biz>
2023-07-15 16:03:11 +0200

    deltas WIP

--- src/main/java/devs/mrp/springturkey/delta/validation/impl/SettingDataConstrainer.java
+++ src/main/java/devs/mrp/springturkey/delta/validation/impl/SettingDataConstrainer.java
@@ -2,6 +2,7 @@ package devs.mrp.springturkey.delta.validation.impl;
 
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import devs.mrp.springturkey.database.service.DeltaFacadeService;
@@ -12,22 +13,27 @@ import devs.mrp.springturkey.delta.validation.FieldValidator;
 @Service("settingConstraints")
 public class SettingDataConstrainer extends DataConstrainerTemplate {
 
+	@Autowired
+	private DeltaFacadeService deltaFacadeService;
+
 	@Override
 	protected boolean isValidTable(DeltaTable table) {
-		// TODO Auto-generated method stub
-		return false;
+		return DeltaTable.SETTING.equals(table);
 	}
 
 	@Override
 	protected Map<String, FieldValidator> getFieldMap() {
-		// TODO Auto-generated method stub
-		return null;
+		return Map.of(
+				"settingValue", FieldValidator.builder()
+				.columnName("setting_value")
+				.predicate(s -> getNamePattern().matcher(s).matches())
+				.build()
+				);
 	}
 
 	@Override
 	protected DeltaFacadeService getDeltaFacadeService() {
-		// TODO Auto-generated method stub
-		return null;
+		return deltaFacadeService;
 	}
 
 }
